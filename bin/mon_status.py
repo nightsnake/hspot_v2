df647fea74602723dcd31386b2fbdfa29cda7051
Alexey Uchakin <auchakin@gmail.com>
2016-11-08 15:37:25 +0300

    typo fixed

--- bin/mon_status.py
+++ bin/mon_status.py
@@ -11,7 +11,7 @@ import os, sys, inspect
 cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"../lib")))
 if cmd_subfolder not in sys.path:
  sys.path.insert(0, cmd_subfolder)
-from devices import *
+from db_devices import *
 from logger import *
 from connector import *
 
